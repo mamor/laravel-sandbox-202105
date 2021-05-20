9184ba14019441d57982b60c7ed7f1a71f2c0412
mamor <mamor.dev@gmail.com>
2021-05-20 23:08:14 +0900

    added "/mail" route for SMTP testing by MailHog

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('mail', function () {
+    Config::set('mail.from.address', 'mamor+from@example.com');
+    Mail::to('mamor+to@example.com')->send(new \App\Mail\OrderShipped());
+});
